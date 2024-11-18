7133632947816354e1042193a5e138d4a3a83680
nikhilagarwal204 <anikhil428@gmail.com>
2024-11-19 03:50:24 +0530

    enable cors, allow all origins testing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -9,6 +10,7 @@ import csv
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
